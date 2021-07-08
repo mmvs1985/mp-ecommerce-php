e5599e5e1f0a3d35e48efa3c1b78deaa46c7f3d2
Marcos viera <mmvs1985@gmail.com>
2021-07-08 01:33:32 -0300

    pruebas

--- webhook.php
+++ webhook.php
@@ -3,10 +3,11 @@
 $input = @file_get_contents("php://input");
 
 file_put_contents(
-    'notificaciones.log',
+    './notificaciones.log',
     $input . PHP_EOL. PHP_EOL,
     FILE_APPEND
 );
+error_log("hello, this is a test!" . $input);
 
 http_response_code(201);
 ?>
